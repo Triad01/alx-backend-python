69a07f3ba30b0e17f93cd21d8726f01f23e826bf
Triad01 <luckypee01@gmail.com>
2024-08-01 18:40:39 +0000

    task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-""" module for task3
-"""
+'''Task 2's module.
+'''
+
+
 import math
 
-def floor(n:float) -> float:
-    """return float
-    """
-    return math.floor(n)
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return math.floor(a)
