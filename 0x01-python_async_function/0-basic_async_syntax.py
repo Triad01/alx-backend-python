16905355dbf99339ce168046be8e1c045a034efc
triad01 <luckypee01@gmail.com>
2024-08-05 13:08:01 +0100

    added type annotation for task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-'''Task 0's module.
-'''
+"""Module includes asynchronous coroutines for task 0
+"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''Waits for a random number of seconds.
-    '''
-    wait_time = random.random() * max_delay
-    await asyncio.sleep(wait_time)
-    return wait_time
+    """Function takes in an integer as argument that waits
+    for a random delay.
+
+    Args:
+        max_delay (int): Maximum delay time in seconds.
+
+    Returns:
+        float: The actual delay time.
+    """
+    delay_time = random.random() * max_delay
+    await asyncio.sleep(delay_time)
+    return delay_time
