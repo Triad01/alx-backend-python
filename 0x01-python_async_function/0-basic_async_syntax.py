e10c1b6e7e7a226e1689fde71e892529d66ea5b4
triad01 <luckypee01@gmail.com>
2024-08-05 12:57:11 +0100

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -15,6 +15,6 @@ async def wait_random(max_delay=10) -> float:
     Returns:
         float: The actual delay time.
     """
-    delay_time = random.uniform(0, max_delay)
+    delay_time = random.random() * max_delay
     await asyncio.sleep(delay_time)
     return delay_time
