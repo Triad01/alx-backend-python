f2f9ca1482a28de5e6e2cef819a36dd8263b3e15
triad01 <luckypee01@gmail.com>
2024-08-05 12:47:50 +0100

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module includes asynchronous coroutines for task 0
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """Function takes in an integer as argument that waits
+    for a random delay.
+
+    Args:
+        max_delay (int): Maximum delay time in seconds.
+
+    Returns:
+        float: The actual delay time.
+    """
+    delay_time = random.uniform(0, max_delay)
+    await asyncio.sleep(delay_time)
+    return delay_time
+
